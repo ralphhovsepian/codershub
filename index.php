a0dc83bf191a35bfa2f29de1e9ee4f823784dacd
Ralph Hovsepian <ralphhovsepian@mail.ru>
2017-07-22 02:21:19 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/style.css">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/7fe550bc59.js"></script>
+<title>Codershub</title>
+</head>
+<body>
+
+<nav class="navbar navbar-light bg-primary">
+  <a id="navhead" href="navbar-brand" href="#">
+    CODERSHUB
+      </a>
+</nav>
+<center>
+
+<div id="signupin" class="container">
+  <br>
+<input type="submit" class="btn btn-outline-primary" value="Login">
+<input type="submit" class="btn btn-outline-success" value="Join now">
+<br><br>
+</div>
+
+  <div id="pageheader" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#pageheader" data-slide-to="0" class="active"></li>
+    <li data-target="#pageheader" data-slide-to="1"></li>
+    <li data-target="#pageheader" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="assets/images/coder.jpg" alt="Coder">
+      <div class="carousel-caption d-none d-md-block">
+      <h3>Codershub</h3>
+      <p>A Great Community for Programmers</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="assets/images/help.jpg" alt="Help">
+      <div class="carousel-caption d-none d-md-block">
+      <h3>Help Each Other!</h3>
+      <p>A good way to learn coding</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="assets/images/join.jpg" alt="Join">
+<div class="carousel-caption d-none d-md-block">
+<h3>Join Codershub now!</h3>
+<p>It's free, and will always be!</p>
+<button type="submit" class="btn btn-success">Join</button>
+</div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#pageheader" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#pageheader" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</center>
+
+</body>
+</html>
